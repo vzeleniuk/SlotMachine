865f7485ee26044f334c73091a411c628f9815d4
Volodymyr Zeleniuk <volodymyr.zeleniuk@bd.com>
2020-07-14 10:56:35 +0300

    Updates

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Reel } from './Reel'
-export { default as DebugArea } from './DebugArea'
-export { Balance } from './Balance'
-export { SpinButton } from './SpinButton'
-export { PayTable } from './PayTable'
