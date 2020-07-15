1d6a7b69482f478771e32af02518e4eb02b1cab0
Volodymyr Zeleniuk <volodymyr.zeleniuk@bd.com>
2020-07-15 19:33:56 +0300

    Updates

---
+++ precache-manifest.957be8987013053aa67bed963bd837c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39c627f3619e44fcc0001e9d699b8d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d5003efc3cc41e674bf",
+    "url": "/static/css/main.c44f390c.chunk.css"
+  },
+  {
+    "revision": "0468f6ad1323e5fd323a",
+    "url": "/static/js/2.19d5a4f9.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.19d5a4f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d5003efc3cc41e674bf",
+    "url": "/static/js/main.73b23b85.chunk.js"
+  },
+  {
+    "revision": "37f34eb4953fc44f7d35",
+    "url": "/static/js/runtime-main.9b4f0c59.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SlotMachine/precache-manifest.761288b4632e88e8f0856b969892156c.js"
+  "/precache-manifest.957be8987013053aa67bed963bd837c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SlotMachine/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
